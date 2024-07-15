3849837a348a3fdc4ea5d036106608990682be81
Nicovel <nicovel98@gmail.com>
2024-07-15 08:59:21 -0500

    Material Skeletons

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
+import Skeleton from '@mui/material/Skeleton';
 
 function App() {
 
@@ -43,43 +42,43 @@ function App() {
   }, []);
 
   return (
-    <>
+    <div className="container">
       {
-      randomCat.length > 0 ? 
-        <div className="card text-center d-flex align-items-center justify-content-center" style={{width: "18rem", height: "32rem", border: "2px solid"}}>
-          <img src={randomCat[0]?.url} className="card-img-top" alt="A random cat" width={200} height={200}/>
-          <div className="card-body">
-            <h5 className="card-title">{randomCat[0].id}</h5>
-            <p className="card-text">
-              <b>Height:</b><br/>
-              {randomCat[0].height}<br/>
-              <b>Width:<br/>
-              </b> {randomCat[0].width}
-            </p>
-            <a className="btn btn-primary" onClick={() => getAnotherRandomCat()}>Randomize</a>
+        randomCat.length > 0 ?
+          <div className="card text-center d-flex align-items-center justify-content-center border-success" style={{ width: "24rem", height: "34rem" }}>
+            <img src={randomCat[0]?.url} className="card-img-top" alt="A random cat" width={250} height={250} />
+            <div className="card-body">
+              <h5 className="card-title">{randomCat[0].id}</h5>
+              <p className="card-text">
+                <b>Height:</b><br />
+                {randomCat[0].height}<br />
+                <b>Width:<br />
+                </b> {randomCat[0].width}
+              </p>
+              <a className="btn btn-primary" onClick={() => getAnotherRandomCat()}>Randomize</a>
+            </div>
           </div>
-        </div>
-      :
-        <div className="card text-center d-flex align-items-center justify-content-center p0" style={{width: "18rem", height: "32rem", border: "2px solid"}}>
-          <div className="card text-center d-flex align-items-center justify-content-center p0" style={{width: "18rem", height: "32rem", border: "none", borderLeft: "2px solid", borderRight: "2px solid"}}>
-            <Skeleton variant="rounded" width={200} height={200} />
+          :
+          <div className="card text-center d-flex align-items-center justify-content-center p-0" style={{ width: "24rem", height: "34rem" }}>
+            <div className="card text-center d-flex align-items-center justify-content-center p-0" style={{ width: "24rem", height: "34rem" }}>
+              <Skeleton variant="rounded" width={260} height={240} />
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">
+                <Skeleton variant="text" sx={{ fontSize: '3rem' }} />
+              </h5>
+              <p className="card-text">
+                <b>Height:</b>
+                <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+                <br />
+                <b>Width:</b>
+                <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+              </p>
+              <a className="btn btn-primary">Loading...</a>
+            </div>
           </div>
-          <div className="card-body">
-            <h5 className="card-title">
-              <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
-            </h5>
-            <p className="card-text">
-              <b>Height:</b>
-              <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
-              <br/>
-              <b>Width:</b>
-              <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
-            </p>
-            <a className="btn btn-primary">Loading...</a>
-          </div>
-        </div>
       }
-    </>
+    </div>
   )
 }
 

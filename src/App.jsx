2b7b913f540eab3dd0c9eb84b0e20ccfc310c0ea
Ingeniando Mentes <ingeniandomentes@gmail.com>
2024-07-15 07:58:09 -0500

    [ADD]: Adding new characteristics for cat random api

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [randomCat, setRandomCat] = useState([]);
+
+  const getAnotherRandomCat = async () => {
+    setRandomCat([]);
+    axios({
+      method: 'get',
+      url: 'https://api.thecatapi.com/v1/images/search',
+    })
+      .then((response) => {
+        console.log(response);
+        setRandomCat(response?.data);
+      })
+      .catch(e => {
+        console.error(e);
+      });
+  };
+
+  useEffect(() => {
+
+    const getRandomCat = async () => {
+      axios({
+        method: 'get',
+        url: 'https://api.thecatapi.com/v1/images/search',
+      })
+        .then((response) => {
+          console.log(response);
+          setRandomCat(response?.data);
+        })
+        .catch(e => {
+          console.error(e);
+        });
+    };
+
+    getRandomCat();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {
+      randomCat.length > 0 ? 
+        <div className="card text-center d-flex align-items-center justify-content-center" style={{width: "18rem", height: "32rem", border: "2px solid"}}>
+          <img src={randomCat[0]?.url} className="card-img-top" alt="A random cat" width={200} height={200}/>
+          <div className="card-body">
+            <h5 className="card-title">{randomCat[0].id}</h5>
+            <p className="card-text">
+              <b>Height:</b><br/>
+              {randomCat[0].height}<br/>
+              <b>Width:<br/>
+              </b> {randomCat[0].width}
+            </p>
+            <a className="btn btn-primary" onClick={() => getAnotherRandomCat()}>Randomize</a>
+          </div>
+        </div>
+      :
+        <div className="card text-center d-flex align-items-center justify-content-center p0" style={{width: "18rem", height: "32rem", border: "2px solid"}}>
+          <div className="card text-center d-flex align-items-center justify-content-center p0" style={{width: "18rem", height: "32rem", border: "none", borderLeft: "2px solid", borderRight: "2px solid"}}>
+            <Skeleton variant="rounded" width={200} height={200} />
+          </div>
+          <div className="card-body">
+            <h5 className="card-title">
+              <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+            </h5>
+            <p className="card-text">
+              <b>Height:</b>
+              <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+              <br/>
+              <b>Width:</b>
+              <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+            </p>
+            <a className="btn btn-primary">Loading...</a>
+          </div>
+        </div>
+      }
     </>
   )
 }
